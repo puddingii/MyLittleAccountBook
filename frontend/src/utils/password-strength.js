66907add4ee487719af2023f90d2426890237353
puddingii <gun4930@naver.com>
2023-08-01 02:04:38 +0900

    chore(join): 회원가입 페이지 한글화 진행

--- frontend/src/utils/password-strength.js
+++ frontend/src/utils/password-strength.js
@@ -1,29 +1,30 @@
 // has number
-const hasNumber = (number) => new RegExp(/[0-9]/).test(number);
+const hasNumber = number => new RegExp(/[0-9]/).test(number);
 
 // has mix of small and capitals
-const hasMixed = (number) => new RegExp(/[a-z]/).test(number) && new RegExp(/[A-Z]/).test(number);
+const hasMixed = number =>
+	new RegExp(/[a-z]/).test(number) && new RegExp(/[A-Z]/).test(number);
 
 // has special chars
-const hasSpecial = (number) => new RegExp(/[!#@$%^&*)(+=._-]/).test(number);
+const hasSpecial = number => new RegExp(/[!#@$%^&*)(+=._-]/).test(number);
 
 // set color based on password strength
-export const strengthColor = (count) => {
-  if (count < 2) return { label: 'Poor', color: 'error.main' };
-  if (count < 3) return { label: 'Weak', color: 'warning.main' };
-  if (count < 4) return { label: 'Normal', color: 'warning.dark' };
-  if (count < 5) return { label: 'Good', color: 'success.main' };
-  if (count < 6) return { label: 'Strong', color: 'success.dark' };
-  return { label: 'Poor', color: 'error.main' };
+export const strengthColor = count => {
+	if (count < 2) return { label: '위험', color: 'error.main' };
+	if (count < 3) return { label: '약함', color: 'warning.main' };
+	if (count < 4) return { label: '보통', color: 'warning.dark' };
+	if (count < 5) return { label: '좋음', color: 'success.main' };
+	if (count < 6) return { label: '강력', color: 'success.dark' };
+	return { label: '위험', color: 'error.main' };
 };
 
 // password strength indicator
-export const strengthIndicator = (number) => {
-  let strengths = 0;
-  if (number.length > 5) strengths += 1;
-  if (number.length > 7) strengths += 1;
-  if (hasNumber(number)) strengths += 1;
-  if (hasSpecial(number)) strengths += 1;
-  if (hasMixed(number)) strengths += 1;
-  return strengths;
+export const strengthIndicator = number => {
+	let strengths = 0;
+	if (number.length > 7) strengths += 1;
+	if (number.length > 9) strengths += 1;
+	if (hasNumber(number)) strengths += 1;
+	if (hasSpecial(number)) strengths += 1;
+	if (hasMixed(number)) strengths += 1;
+	return strengths;
 };
---
+++ frontend/src/validation/user.js
@@ -0,0 +1,21 @@
+import { object, string } from 'yup';
+
+const userSchema = object().shape({
+	nickname: string().max(255).required('닉네임은 필수 입력 항목입니다.'),
+	email: string()
+		.email('이메일 형식이 아닙니다.')
+		.max(255)
+		.required('이메일은 필수 입력 항목입니다.'),
+	password: string()
+		.max(255)
+		.matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&].{7,}$/, {
+			message:
+				'최소 8자 이상, 각각 최소 하나의 대/소문자, 숫자, 특수 문자가 포함되어야 합니다.',
+		})
+		.required('비밀번호는 필수 입력 항목입니다.'),
+});
+
+const loginSchema = userSchema.pick(['email', 'password']);
+const joinSchema = userSchema.clone();
+
+export { userSchema, loginSchema, joinSchema };
