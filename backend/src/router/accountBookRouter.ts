dcce2c9b98f0f3cbdc7e14ff17c999b538209758
puddingii <gun4930@naver.com>
2023-08-21 03:05:24 +0900

    chore(validation): 가계부 내용 작성시 사용하는 Api를 위한 검증 로직 작성 및 검증 테스트

--- backend/src/router/accountBookRouter.ts
+++ backend/src/router/accountBookRouter.ts
@@ -7,6 +7,7 @@ import { convertErrorToCustomError } from '@/util/error';
 
 import { verifyToken } from '@/middleware/authentication';
 import { getCategory } from '@/service/accountBookService';
+import { COLUMN_WRITE_TYPE } from '@/util/parser/schema/accountBookSchema';
 
 import { TGetCategory } from '@/interface/api/accountBookResponse';
 
@@ -23,6 +24,30 @@ router.get('/category', verifyToken, async (req, res) => {
 		return res
 			.status(200)
 			.json({ data: list, message: '', status: 'success' } as TGetCategory);
+	} catch (error) {
+		const { message, traceList, code } = convertErrorToCustomError(error, {
+			trace: 'Router',
+			code: 400,
+		});
+		logger.error(message, traceList);
+
+		return res.status(code).json({ data: {}, message, status: 'fail' });
+	}
+});
+
+router.post('/column', verifyToken, async (req, res) => {
+	try {
+		const columnInfo = await zParser(zodSchema.accountBook.postColumn, req.body);
+
+		if (columnInfo.writeType === COLUMN_WRITE_TYPE.FIXED) {
+			console.log('Fixed ');
+		} else {
+			console.log('Not Fixed');
+		}
+
+		return res
+			.status(200)
+			.json({ data: {}, message: '', status: 'success' } as TGetCategory);
 	} catch (error) {
 		const { message, traceList, code } = convertErrorToCustomError(error, {
 			trace: 'Router',
--- backend/src/util/parser/schema/accountBookSchema.ts
+++ backend/src/util/parser/schema/accountBookSchema.ts
@@ -6,4 +6,56 @@ const getCategory = zod.object({
 	}),
 });
 
-export default { getCategory };
+export const COLUMN_WRITE_TYPE: { FIXED: 'f'; NOTFIXED: 'nf' } = {
+	FIXED: 'f',
+	NOTFIXED: 'nf',
+};
+
+const publicColumn = zod.object({
+	value: zod.number().min(1, '1원 이상 입력해야 합니다.'),
+	type: zod.enum(['income', 'spending'], {
+		required_error: '지출/수입 타입 정보가 필요합니다.',
+		invalid_type_error: '지출 혹은 수입의 타입 정보가 아닙니다.',
+	}),
+	category: zod.number().min(0, '카테고리는 필수 항목입니다.'),
+	content: zod.string().optional(),
+});
+
+const notFixedColumn = publicColumn.extend({
+	writeType: zod.literal(COLUMN_WRITE_TYPE.NOTFIXED),
+	spendingAndIncomeDate: zod.string().datetime({ message: '날짜 형식이 아닙니다.' }),
+});
+
+const fixedColumn = publicColumn.extend({
+	writeType: zod.literal(COLUMN_WRITE_TYPE.FIXED),
+	cycleTime: zod.number().min(0, '0 이하로 입력할 수 없습니다.'),
+	cycleType: zod.enum(['sd', 'd', 'w', 'm', 'y'], {
+		required_error: '고정주기 입력 시 필수 항목입니다.',
+		invalid_type_error: '알 수 없는 주기입니다.',
+	}),
+});
+
+const postColumn = zod
+	.discriminatedUnion('writeType', [notFixedColumn, fixedColumn])
+	.refine(
+		data => {
+			if (data.writeType === COLUMN_WRITE_TYPE.FIXED) {
+				const { cycleTime, cycleType } = data;
+				return (
+					(cycleType === 'sd' && cycleTime <= 28) ||
+					(cycleType !== 'sd' && cycleTime <= 365)
+				);
+			}
+			return true;
+		},
+		{
+			message:
+				'특정일 선택할 시 주기 날짜 <= 28일 / 특정일이 아닌 경우 주기 날짜 <= 365일',
+			path: ['cycleTime'],
+		},
+	);
+
+export type TGetCategoryQuery = zod.infer<typeof getCategory>;
+export type TPostColumnQuery = zod.infer<typeof postColumn>;
+
+export default { getCategory, postColumn };
