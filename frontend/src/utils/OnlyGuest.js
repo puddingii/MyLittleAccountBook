7b172a15ef5f6760ec64569c36d5881d5a1069f1
puddingii <gun4930@naver.com>
2023-09-04 01:47:57 +0900

    chore(middleware): Front쪽 Only Guest 미들웨어 로직 수정

--- frontend/src/utils/OnlyGuest.js
+++ frontend/src/utils/OnlyGuest.js
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router';
-import { getCookie } from './cookie';
 import { useEffect } from 'react';
 import axios from 'axios';
 
+import { useRefreshAccessTokenQuery } from 'queries/auth/authQuery';
+import { getCookie } from './cookie';
+
 const OnlyGuest = ({ children }) => {
 	const navigate = useNavigate();
+	const { isError, isFetched } = useRefreshAccessTokenQuery();
 
 	useEffect(() => {
 		const token = getCookie('token');
 		const access = axios.defaults.headers.common.Authorization;
 
-		if (token && access) {
+		if (token && access && isFetched && !isError) {
 			navigate(-1);
 		}
 	}, []);
