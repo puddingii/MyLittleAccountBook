3e3c4842ada70355652a6d5ba6313d7bff4a4c1d
puddingii <gun4930@naver.com>
2023-10-27 23:17:41 +0900

    feat(test): zod schema(accountBook) 테스트 코드 덜 작성 되었던 부분 작성

--- backend/src/test/schema/accountBook.spec.ts
+++ backend/src/test/schema/accountBook.spec.ts
@@ -7,7 +7,10 @@ import { ParseParams } from 'zod';
 import schema from '@/util/parser/schema';
 import zParser from '@/util/parser';
 import {
+	TDeleteColumnQuery,
 	TGetCategoryQuery,
+	TGetColumnListQuery,
+	TGetSummaryQuery,
 	TPatchColumnQuery,
 	TPatchFixedColumnQuery,
 	TPatchNotFixedColumnQuery,
@@ -692,6 +695,214 @@ describe('AccountBook Zod Schema Test', function () {
 				];
 
 				requestList.forEach(request => zParser(accountBookSchema.patchColumn, request));
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const fulfilledIdx = resultList.findIndex(
+					result => result.status === 'fulfilled',
+				);
+				if (fulfilledIdx > -1) {
+					throw new Error(`index: ${fulfilledIdx} is error`);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+	});
+
+	describe('#getColumnList', function () {
+		let spyZParser: sinon.SinonSpy<
+			[data: unknown, params?: Partial<ParseParams> | undefined],
+			Promise<TGetColumnListQuery>
+		>;
+
+		beforeEach(function () {
+			spyZParser = sinon.spy(accountBookSchema.getColumnList, 'parseAsync');
+		});
+
+		it('Check correct', async function () {
+			try {
+				const requestList = [
+					{ accountBookId: '1', startDate: '2022-02-02', endDate: '2002-02-02' },
+					{ accountBookId: '1qwe', startDate: '2022-02-02', endDate: '2002-02-02' },
+				];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.getColumnList, { query: request }),
+				);
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const rejectedIdx = resultList.findIndex(result => result.status === 'rejected');
+				if (rejectedIdx > -1) {
+					throw new Error(
+						`index: ${rejectedIdx} - ${
+							(resultList[rejectedIdx] as PromiseRejectedResult).reason
+						}`,
+					);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check wrong', async function () {
+			try {
+				const requestList = [
+					{ accountBookId: 1, startDate: '2022-02-02', endDate: '2002-02-02' },
+					{ accountBookId: {}, startDate: '2022-02-02', endDate: '2002-02-02' },
+					{ startDate: '2022-02-02', endDate: '2002-02-02' },
+					{ accountBookId: '1', startDate: {}, endDate: '2002-02-02' },
+					{ accountBookId: '1', startDate: 1, endDate: '2002-02-02' },
+					{ accountBookId: '1', endDate: '2002-02-02' },
+					{ accountBookId: '1', startDate: '2022-02-02', endDate: {} },
+					{ accountBookId: '1', startDate: '2022-02-02', endDate: 1 },
+					{ accountBookId: '1', startDate: '2022-02-02' },
+					{ accountBookId: '1' },
+					{ startDate: '2022-02-02' },
+					{ endDate: '2022-02-02' },
+					{},
+				];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.getColumnList, { query: request }),
+				);
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const fulfilledIdx = resultList.findIndex(
+					result => result.status === 'fulfilled',
+				);
+				if (fulfilledIdx > -1) {
+					throw new Error(`index: ${fulfilledIdx} is error`);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+	});
+
+	describe('#deleteColumn', function () {
+		let spyZParser: sinon.SinonSpy<
+			[data: unknown, params?: Partial<ParseParams> | undefined],
+			Promise<TDeleteColumnQuery>
+		>;
+
+		beforeEach(function () {
+			spyZParser = sinon.spy(accountBookSchema.deleteColumn, 'parseAsync');
+		});
+
+		it('Check correct', async function () {
+			try {
+				const requestList = [
+					{ id: '1', writeType: 'nf' },
+					{ id: '1', writeType: 'f' },
+					{ id: '1qwe', writeType: 'nf' },
+					{ id: '1qwe', writeType: 'f' },
+				];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.deleteColumn, { query: request }),
+				);
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const rejectedIdx = resultList.findIndex(result => result.status === 'rejected');
+				if (rejectedIdx > -1) {
+					throw new Error(
+						`index: ${rejectedIdx} - ${
+							(resultList[rejectedIdx] as PromiseRejectedResult).reason
+						}`,
+					);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check wrong', async function () {
+			try {
+				const requestList = [
+					{ id: '1', writeType: 'a' },
+					{ id: '1', writeType: '' },
+					{ id: '1', writeType: {} },
+					{ id: '1', writeType: 1 },
+					{ id: '1' },
+					{ id: 1, writeType: 'nf' },
+					{ id: {}, writeType: 'nf' },
+					{ writeType: 'nf' },
+					{},
+				];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.deleteColumn, { query: request }),
+				);
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const fulfilledIdx = resultList.findIndex(
+					result => result.status === 'fulfilled',
+				);
+				if (fulfilledIdx > -1) {
+					throw new Error(`index: ${fulfilledIdx} is error`);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+	});
+
+	describe('#getSummary', function () {
+		let spyZParser: sinon.SinonSpy<
+			[data: unknown, params?: Partial<ParseParams> | undefined],
+			Promise<TGetSummaryQuery>
+		>;
+
+		beforeEach(function () {
+			spyZParser = sinon.spy(accountBookSchema.getSummary, 'parseAsync');
+		});
+
+		it('Check correct', async function () {
+			try {
+				const requestList = [
+					{ accountBookId: '123123' },
+					{ accountBookId: 'a' },
+					{ accountBookId: '#@' },
+					{ accountBookId: '1' },
+				];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.getSummary, { query: request }),
+				);
+				const resultList = await Promise.allSettled(
+					spyZParser.getCalls().map(call => call.returnValue),
+				);
+
+				const rejectedIdx = resultList.findIndex(result => result.status === 'rejected');
+				if (rejectedIdx > -1) {
+					throw new Error(
+						`index: ${rejectedIdx} - ${
+							(resultList[rejectedIdx] as PromiseRejectedResult).reason
+						}`,
+					);
+				}
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check wrong', async function () {
+			try {
+				const requestList = [{ accountBookId: 1 }, { accountBookId: {} }, {}];
+
+				requestList.forEach(request =>
+					zParser(accountBookSchema.getSummary, { query: request }),
+				);
 				const resultList = await Promise.allSettled(
 					spyZParser.getCalls().map(call => call.returnValue),
 				);
