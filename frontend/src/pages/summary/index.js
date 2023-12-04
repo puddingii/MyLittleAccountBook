951a206f00bad6ada3f61a5054d3ba8c97bda81f
puddingii <gun4930@naver.com>
2023-12-05 03:09:52 +0900

    feat(Summary): socket 이벤트 버그 수정 및 삭제 관련 로직 추가
    1. useRef를 사용한 소켓관리로 변경.
    2. 이벤트 Callback에서 useState로 관리되는 객체가 있기 때문에 이벤트를 따로 useEffect로 관리(각 Dependency에 맞게 리랜더링 하도록 설정)
    3. 삭제 이벤트 추가(not fixed, fixed column 둘다 추가)

--- frontend/src/pages/summary/index.js
+++ frontend/src/pages/summary/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router';
 
 // material-ui
@@ -15,9 +15,39 @@ import dayjs from 'dayjs';
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
+const findCategory = (list, gabId) => {
+	const idx = list.findIndex(category => category.gabId === gabId);
+	return idx === -1 ? idx : [idx];
+};
+
+const findDeepCategory = (list, gabId) => {
+	const listLength = list.length;
+	for (let i = 0; i < listLength; i++) {
+		const categoryListLength = list[i].length;
+		for (let j = 0; j < categoryListLength; j++) {
+			if (list[i][j].gabId === gabId) {
+				return [i, j];
+			}
+		}
+	}
+
+	return -1;
+};
+
+const findCategorys = (findFunc, list, list2, gabId) => {
+	const result = findFunc(list, gabId);
+	if (result === -1) {
+		const result2 = findFunc(list2, gabId);
+		return result2 === -1 ? result2 : [...result2, 2];
+	}
+
+	return [...result, 1];
+};
+
 const ThisMonthSummary = () => {
 	const param = useParams();
 	const accountBookId = parseInt(param?.id ?? -1, 10);
+	const socketRef = useRef(null);
 	const [notFixedIncomeList, setNotFixedIncomeList] = useState([]);
 	const [notFixedSpendingList, setNotFixedSpendingList] = useState([]);
 	const [fixedIncomeList, setFixedIncomeList] = useState([]);
@@ -37,28 +67,74 @@ const ThisMonthSummary = () => {
 		},
 	);
 
-	const addNotFixedColumn = column => {
-		const curDate = dayjs(column.spendingAndIncomeDate).date() - 1;
-		const setter = column.type === 'income' ? setNotFixedIncomeList : setNotFixedSpendingList;
+	const addNotFixedColumn = useCallback(
+		column => {
+			const curDate = dayjs(column.spendingAndIncomeDate).date() - 1;
+			const setter = column.type === 'income' ? setNotFixedIncomeList : setNotFixedSpendingList;
 
-		setter(columnList => {
-			const deepClone = JSON.parse(JSON.stringify(columnList));
-			deepClone[curDate].push(column);
+			setter(columnList => {
+				const deepClone = JSON.parse(JSON.stringify(columnList));
+				deepClone[curDate].push(column);
 
-			return deepClone;
-		});
-	};
+				return deepClone;
+			});
+		},
+		[setNotFixedIncomeList, setNotFixedSpendingList],
+	);
 
-	const addFixedColumn = column => {
-		const setter = column.type === 'income' ? setFixedIncomeList : setFixedSpendingList;
+	const addFixedColumn = useCallback(
+		column => {
+			const setter = column.type === 'income' ? setFixedIncomeList : setFixedSpendingList;
 
-		setter(columnList => {
-			const deepClone = [...columnList];
-			deepClone.push(column);
+			setter(columnList => {
+				const deepClone = [...columnList];
+				deepClone.push(column);
 
-			return deepClone;
-		});
-	};
+				return deepClone;
+			});
+		},
+		[setFixedIncomeList, setFixedSpendingList],
+	);
+
+	const deleteNotFixedColumn = useCallback(
+		gabId => {
+			const result = findCategorys(findDeepCategory, notFixedIncomeList, notFixedSpendingList, gabId);
+
+			if (result === -1) {
+				return;
+			}
+			const [x, y, num] = result;
+			const setter = num === 1 ? setNotFixedIncomeList : setNotFixedSpendingList;
+
+			setter(columnList => {
+				const deepClone = JSON.parse(JSON.stringify(columnList));
+				deepClone[x].splice(y, 1);
+
+				return deepClone;
+			});
+		},
+		[notFixedIncomeList, notFixedSpendingList],
+	);
+
+	const deleteFixedColumn = useCallback(
+		gabId => {
+			const result = findCategorys(findCategory, fixedIncomeList, fixedSpendingList, gabId);
+
+			if (result === -1) {
+				return;
+			}
+			const [index, num] = result;
+			const setter = num === 1 ? setFixedIncomeList : setFixedSpendingList;
+
+			setter(columnList => {
+				const deepClone = [...columnList];
+				deepClone.splice(index, 1);
+
+				return deepClone;
+			});
+		},
+		[fixedIncomeList, fixedSpendingList],
+	);
 
 	useEffect(() => {
 		refetch();
@@ -66,21 +142,56 @@ const ThisMonthSummary = () => {
 
 	useEffect(() => {
 		const socket = getSocket(accountBookId);
-		socket.on('create:nfgab', info => {
-			addNotFixedColumn(info);
-		});
-		socket.on('create:fgab', info => {
-			addFixedColumn(info);
-		});
+		socketRef.current = socket;
 
-		socket.connect();
+		socketRef.current.connect();
 
 		return () => {
-			if (accountBookId && socket.active) {
-				socket.disconnect();
+			if (accountBookId && socketRef.current.active) {
+				socketRef.current.disconnect();
 			}
 		};
 	}, [accountBookId]);
+
+	useEffect(() => {
+		const addNFHandler = info => {
+			addNotFixedColumn(info);
+		};
+		const addFHandler = info => {
+			addFixedColumn(info);
+		};
+		const updateNFHandler = info => {
+			console.log(info);
+		};
+		const updateFHandler = info => {
+			console.log(info);
+		};
+		const deleteNFHandler = info => {
+			deleteNotFixedColumn(info.id);
+		};
+		const deleteFHandler = info => {
+			deleteFixedColumn(info.id);
+		};
+		if (socketRef.current) {
+			socketRef.current.on('create:nfgab', addNFHandler);
+			socketRef.current.on('create:fgab', addFHandler);
+			socketRef.current.on('update:nfgab', updateNFHandler);
+			socketRef.current.on('update:fgab', updateFHandler);
+			socketRef.current.on('delete:nfgab', deleteNFHandler);
+			socketRef.current.on('delete:fgab', deleteFHandler);
+		}
+
+		return () => {
+			if (socketRef.current) {
+				socketRef.current.off('create:nfgab', addNFHandler);
+				socketRef.current.off('create:fgab', addFHandler);
+				socketRef.current.off('update:nfgab', updateNFHandler);
+				socketRef.current.off('update:fgab', updateFHandler);
+				socketRef.current.off('delete:nfgab', deleteNFHandler);
+				socketRef.current.off('delete:fgab', deleteFHandler);
+			}
+		};
+	}, [socketRef, deleteNotFixedColumn, deleteFixedColumn, addNotFixedColumn, addFixedColumn]);
 
 	return (
 		<Grid container rowSpacing={4.5} columnSpacing={2.75}>
