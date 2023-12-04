2674030a5b4d5ac56d4b80d3493d3e805e353108
puddingii <gun4930@naver.com>
2023-12-04 23:02:06 +0900

    test(loadtest): 부하 테스트 stage 및 확인 옵션 추가

--- backend/k6/test.js
+++ backend/k6/test.js
@@ -1,21 +1,36 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, group, check } from 'k6';
 
 export const options = {
 	stages: [
 		{ duration: '30s', target: 20 },
-		{ duration: '1m30s', target: 10 },
-		{ duration: '30s', target: 0 },
-		{ duration: '1m30s', target: 0 },
+		{ duration: '1m30s', target: 30 },
+		{ duration: '30s', target: 40 },
+		{ duration: '1m', target: 5 },
+		{ duration: '30s', target: 10 },
+		{ duration: '2m', target: 50 },
 	],
+	thresholds: {
+		http_req_failed: ['rate<0.01'],
+		http_req_duration: ['p(95)<300'],
+	},
 };
 
 export default function () {
-	http.get('http://localhost:3044/accountbook/summary?accountBookId=1', {
-		headers: {
-			authorization: 'Enter the Access Token',
-			refresh: 'Enter the Refresh Token',
-		},
+	const headers = {
+		authorization: '',
+		refresh: '',
+	};
+
+	group('summary', function () {
+		const res = http.get('http://localhost:3044/accountbook/summary?accountBookId=1', {
+			headers,
+		});
+
+		check(res, {
+			'response code was 200': res => res.status === 200,
+		});
 	});
+
 	sleep(1);
 }
