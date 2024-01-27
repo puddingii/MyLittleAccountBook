08e2369c70e1e407b9e39f88c93001b673165763
puddingii <gun4930@naver.com>
2024-01-27 23:16:38 +0900

    chore(test): github actions 테스트용도

--- backend/src/test/util/date/index.spec.ts
+++ backend/src/test/util/date/index.spec.ts
@@ -11,9 +11,64 @@ describe('Date Util Test', function () {
 	const curDate = dayjs().toDate();
 
 	describe('#calculateNextCycle', function () {
-		it('Check expected result', function () {
+		it('Check d', function () {
 			try {
 				const result = DateUtil.calculateNextCycle(curDate, 1, 'd');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check w', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'w');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check m', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'm');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check y', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'd');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check d', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'd');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+
+		it('Check sd', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'sd');
 
 				/** FIXME 테스트용 일부러 틀려놓음 */
 				equal(curDate.getDate(), result.getDate());
