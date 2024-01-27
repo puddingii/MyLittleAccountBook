8617720af38fb26640a3edf52c8360126529b6dc
puddingii <gun4930@naver.com>
2024-01-27 21:37:36 +0900

    chore(test): Schema 테스트 코드 폴더 변경

--- backend/.mocharc.js
+++ backend/.mocharc.js
@@ -3,7 +3,7 @@ var isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
 	extension: ['ts'],
 	spec: isProduction
-		? ['src/test/service/**/*.spec.ts', 'src/test/schema/**/*.spec.ts']
+		? ['src/test/service/**/*.spec.ts', 'src/test/util/**/*.spec.ts']
 		: 'src/test/**/*.spec.ts',
 	require: ['ts-node/register', 'src/test'],
 	exit: true,
---
+++ backend/src/test/util/date/index.spec.ts
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AssertionError, equal, fail, ok } from 'assert';
+import sinon from 'sinon';
+import dayjs from 'dayjs';
+
+/** Date util */
+import DateUtil from '@/util/date';
+
+describe('Date Util Test', function () {
+	const curDate = dayjs().toDate();
+
+	describe('#calculateNextCycle', function () {
+		it('Check function parameters', function () {
+			try {
+				const result = DateUtil.calculateNextCycle(curDate, 1, 'd');
+
+				/** FIXME 테스트용 일부러 틀려놓음 */
+				equal(curDate.getDate(), result.getDate());
+			} catch (err) {
+				fail(err as Error);
+			}
+		});
+	});
+});
