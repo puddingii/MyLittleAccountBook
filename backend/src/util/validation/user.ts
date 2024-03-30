c2ff639098bc05c01e22ff3b7150425aa3b8856c
puddingii <gun4930@naver.com>
2024-03-30 17:45:23 +0900

    refactor(validation): zod를 사용한 방식으로 변경

--- backend/src/util/validation/user.ts
+++ backend/src/util/validation/user.ts
@@ -1,13 +1,25 @@
-import GroupModel from '@/model/group';
+import * as zod from 'zod';
+
+import type GroupModel from '@/model/group';
 
 export const isAdminUser = (userType: GroupModel['userType']) => {
-	const adminList = ['owner', 'manager'];
+	const result = zod
+		.enum(['owner', 'manager'], {
+			required_error: '소셜 로그인의 종류 정보가 필요합니다.',
+			invalid_type_error: '구글, 네이버 소셜 로그인만 가능합니다.',
+		})
+		.safeParse(userType);
 
-	return adminList.findIndex(adminType => adminType === userType) !== -1;
+	return result.success;
 };
 
 export const canUserWrite = (userType: GroupModel['userType']) => {
-	const adminList = ['owner', 'manager', 'writer'];
+	const result = zod
+		.enum(['owner', 'manager', 'writer'], {
+			required_error: '소셜 로그인의 종류 정보가 필요합니다.',
+			invalid_type_error: '구글, 네이버 소셜 로그인만 가능합니다.',
+		})
+		.safeParse(userType);
 
-	return adminList.findIndex(adminType => adminType === userType) !== -1;
+	return result.success;
 };
