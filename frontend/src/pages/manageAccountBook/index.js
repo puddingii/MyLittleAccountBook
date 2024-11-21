154cb4f6c1672c8ba3aaf7080c40811872b74009
geckosboy <geckosboy@verywords.com>
2024-11-22 01:34:15 +0900

    test: url check

--- frontend/src/pages/manageAccountBook/index.js
+++ frontend/src/pages/manageAccountBook/index.js
@@ -17,6 +17,7 @@ const ManageAccountBook = () => {
 			onSuccess: response => {
 				const imageRoot = response?.data?.imagePath ? process.env.REACT_APP_IMAGE_GET_SERVER : '';
 				const imagePath = `${imageRoot}/${response?.data?.imagePath ?? ''}`;
+				console.log('image_server', process.env.REACT_APP_IMAGE_GET_SERVER, imagePath);
 				setAccountBookInfo({
 					title: response?.data?.title ?? '',
 					content: response?.data?.content ?? '',
